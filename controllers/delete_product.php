0d1a8f5cc87d67a9a9ac07a0b5466822c86b75c5
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-12-09 15:28:21 +0700

    fixed

--- controllers/delete_product.php
+++ controllers/delete_product.php
@@ -3,6 +3,6 @@
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
        $pdo = db_connect();
-       $pdo->exec("DELETE FROM `products` WHERE `id` ='".$_POST['id']."';");
+       $pdo->exec("DELETE FROM `products` WHERE `id` ='".$_POST['remove-id']."';");
        header("Location: ../views/list_product.php?message=success");
     }
--- views/list_product.php
+++ views/list_product.php
@@ -120,7 +120,7 @@
       </div>
       <div class="modal-footer">
         <form action="../controllers/delete_product.php" method="post">
-            <input name="remove-id" id="remove-id" type="hidden">
+            <input name="remove-id" id="remove-id" type="">
             <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-x-octagon"></i> Cancel</button>
             <button type="submit" class="btn btn-danger" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal" data-bs-dismiss="modal"> <i class="bi bi-trash3"></i> Confirm Delete</button>
         </form>
